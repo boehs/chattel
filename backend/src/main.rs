92ba7be74cf9496b0085ed97a23404c0da517afa
Evan Boehs <evan@boehs.org>
2023-04-23 14:22:11 -0400

    restart one more time

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,2 @@
-use std::error::Error;
-
-use migration::{Migrator, MigratorTrait};
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let connection = sea_orm::Database::connect("sqlite://sqlite.db").await?;
-    Migrator::up(&connection, None).await?;
-
-    Ok(())
+fn main(){
 }
