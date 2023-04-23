997bcc40d6d632884dc3e3c53dae60925570d518
Evan Boehs <evan@boehs.org>
2023-04-23 15:46:57 -0400

    ahhh

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -1,20 +1,6 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    item (id) {
-        id -> Integer,
-        when -> Nullable<Text>,
-        deadline -> Nullable<Text>,
-        parent -> Nullable<Integer>,
-        title -> Text,
-        body -> Nullable<Text>,
-        #[sql_name = "type"]
-        type_ -> Nullable<Text>,
-        status -> Nullable<Text>,
-    }
-}
-
-diesel::table! {
     items (id) {
         id -> Integer,
         when_type -> Nullable<Integer>,
@@ -29,22 +15,6 @@ diesel::table! {
 }
 
 diesel::table! {
-    seaql_migrations (version) {
-        version -> Text,
-        applied_at -> Integer,
-    }
-}
-
-diesel::table! {
-    timeblock (id) {
-        id -> Integer,
-        item -> Nullable<Integer>,
-        start -> Nullable<Text>,
-        end -> Nullable<Text>,
-    }
-}
-
-diesel::table! {
     timeblocks (id) {
         id -> Integer,
         item -> Nullable<Integer>,
@@ -53,13 +23,9 @@ diesel::table! {
     }
 }
 
-diesel::joinable!(timeblock -> item (item));
 diesel::joinable!(timeblocks -> items (item));
 
 diesel::allow_tables_to_appear_in_same_query!(
-    item,
     items,
-    seaql_migrations,
-    timeblock,
     timeblocks,
 );
